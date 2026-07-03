0fa7c5cfc5e2687478961b5da7d1190e844b8fe6
Saritasinha123 <saritasinha.heartypaws@gmail.com>
2026-07-03 19:41:19 +0530

    Refactor PawLogo component for improved readability

--- src/components/PawLogo.tsx
+++ src/components/PawLogo.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
-
-const pawLogoImg = "/src/assets/images/Hearty Paws Logo.png";
+import React from "react";
+import pawLogoImg from "../assets/images/Hearty Paws Logo.png";
 
 interface PawLogoProps {
   className?: string;
   isDarkBackground?: boolean;
 }
 
-export default function PawLogo({ className = "w-5 h-5", isDarkBackground = false }: PawLogoProps) {
+export default function PawLogo({
+  className = "w-5 h-5",
+  isDarkBackground = false,
+}: PawLogoProps) {
   return (
     <img
       src={pawLogoImg}
       alt="Hearty Paws Logo"
-      width="24"
-      height="24"
+      width={24}
+      height={24}
       decoding="async"
-      className={`${className} object-contain ${isDarkBackground ? 'brightness-0 invert' : ''}`}
-      referrerPolicy="no-referrer"
+      className={`${className} object-contain ${
+        isDarkBackground ? "brightness-0 invert" : ""
+      }`}
     />
   );
 }
-
